9d5e76a81bc4da698e82535ffb150f592979a00c
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2023-05-14 21:24:38 -0300

    app
    app

---
+++ config.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from urllib import parse
+from dotenv import load_dotenv
+from os import path
+
+
+load_dotenv()
+
+
+
+myboxuser_mssql = os.getenv('myboxmsqluser')
+myboxpassword_mssql = os.getenv('myboxmsqlpassword')
+myboxdatabase_mssql = os.getenv('myboxmsqldatabase')
+myboxhost_mssql = os.getenv('myboxmmsqlhost')
+
+user_mssql = os.getenv('serveruser')
+password_mssql = os.getenv('serverpassword')
+database_mssql = os.getenv('serverdatabase')
+host_mssql = os.getenv('serverhost')
+
+
+
+def mssql_get_conn():
+
+    connection_url = URL.create(
+            "mssql+pyodbc",
+            username="sa",
+            password="123",
+            host="Guilherme",
+            database="Ecommerce",
+            query={
+                "driver": "ODBC Driver 17 for SQL Server",
+                "autocommit": "True",
+        },
+        )
+      
+    engine = create_engine(connection_url).execution_options(
+    isolation_level="AUTOCOMMIT", future=True,fast_executemany=True)
+    return engine
+
